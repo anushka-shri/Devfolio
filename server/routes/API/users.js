d51459fe7fc01b40cf46ffb73798adc2ad942927
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-08-23 01:57:41 +0530

    user model

--- server/routes/API/users.js
+++ server/routes/API/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('users  route');
-});
+
+router.post('/', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ const connectDB = require('./config/db.js');
 const app = express();
 connectDB();
 
-
-
+// middleware, instead of using body parser
+app.use(express.json({
+    extended: false,
+}));
+ 
 app.get('/', (req, res) => {
     res.send('API running');
 });
 
 app.use('/api/users', require('./routes/API/users'));
+app.use('/api/auth', require('./routes/API/auth'));
+app.use('/api/profile', require('./routes/API/profile'));
+app.use('/api/posts', require('./routes/API/posts'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Serving on port ${PORT}` ))
