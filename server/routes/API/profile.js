9e14fe9503cba5170be5fa68ee68cd6d18bd3bd6
Anushka Shrivastava <anushka.shrivastavaa@gmail.com>
2021-09-01 05:04:09 +0530

    delete exp

--- server/routes/API/profile.js
+++ server/routes/API/profile.js
@@ -153,4 +153,90 @@ router.get('/user/:user_id', async (req, res) => {
 	}
 });
 
+// #route DELETE api/profile
+// #desc delete user and profile
+// #access Private
+
+router.delete('/', auth, async (req, res) => {
+	try {
+		//remove users' post
+		//Remove profile
+		await Profile.findOneAndRemove({ user: req.user.id });
+		// Remove user
+		await User.findOneAndRemove({ _id: req.user.id });
+
+		res.json({ msg: 'User deleted successfully' });
+	} catch (error) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
+// #route PUT api/profile/experience
+// #desc Add profile experience
+// #access Private
+
+router.put(
+	'/experience',
+	[
+		auth,
+		[
+			check('title', 'Title is required').not().isEmpty(),
+			check('company', 'company is required').not().isEmpty(),
+			check('from', 'From date is required').not().isEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const { title, company, location, from, to, current, description } =
+			req.body;
+
+		const newExp = {
+			title,
+			company,
+			location,
+			from,
+			to,
+			current,
+			description,
+		};
+
+		try {
+			const profile = await Profile.findOne({ user: req.user.id });
+            profile.experience.unshift(newExp);
+			await profile.save();
+			res.json(profile);
+		} catch (error) {
+			console.error(err.message);
+			res.status(500).send('Server error');
+		}
+	},
+);
+
+// #route DELETE api/profile/experience/:exp_id
+// #desc delete experience
+// #access Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+	try {
+		
+		//Remove experience
+		const profile = await Profile.findOne({ user: req.user.id });
+	
+		const removeIndex = profile.experience.map(e => e.id)
+			.indexOf(req.params.exp_id);
+		
+		profile.experien
+
+		res.json({ msg: 'User deleted successfully' });
+	} catch (error) {
+		console.error(err.message);
+		res.status(500).send('Server error');
+	}
+});
+
 module.exports = router;
